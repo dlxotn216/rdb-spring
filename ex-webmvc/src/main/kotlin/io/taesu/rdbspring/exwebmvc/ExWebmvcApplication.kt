6c55289b9af70452db3e8887a0baa64ae2aabf52
taesu <taesu@crscube.co.kr>
2024-06-17 16:36:57 +0900

    Initial commit

---
+++ ex-webmvc/src/main/kotlin/io/taesu/rdbspring/exwebmvc/ExWebmvcApplication.kt
@@ -0,0 +1,11 @@
+package io.taesu.rdbspring.exwebmvc
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ExWebmvcApplication
+
+fun main(args: Array<String>) {
+	runApplication<ExWebmvcApplication>(*args)
+}
---
+++ ex-webmvc/src/test/kotlin/io/taesu/rdbspring/exwebmvc/ExWebmvcApplicationTests.kt
@@ -0,0 +1,13 @@
+package io.taesu.rdbspring.exwebmvc
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ExWebmvcApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
