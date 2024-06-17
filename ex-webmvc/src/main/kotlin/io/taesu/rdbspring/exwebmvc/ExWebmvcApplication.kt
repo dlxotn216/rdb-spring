5a1e64fe74f534a3050032e56cdda4351063080e
taesu <taesu@crscube.co.kr>
2024-06-17 17:09:38 +0900

    Initial commit
    default timezone config

--- ex-webmvc/src/main/kotlin/io/taesu/rdbspring/exwebmvc/ExWebmvcApplication.kt
+++ ex-webmvc/src/main/kotlin/io/taesu/rdbspring/exwebmvc/ExWebmvcApplication.kt
@@ -1,11 +1,22 @@
 package io.taesu.rdbspring.exwebmvc
 
+import jakarta.annotation.PostConstruct
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan
 import org.springframework.boot.runApplication
+import java.time.ZoneId
+import java.util.*
 
+@ConfigurationPropertiesScan
 @SpringBootApplication
-class ExWebmvcApplication
+class ExWebmvcApplication {
+	 @PostConstruct
+    fun onConstruct() {
+        TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("UTC")))
+    }
+}
 
 fun main(args: Array<String>) {
+	TimeZone.setDefault(TimeZone.getTimeZone(ZoneId.of("UTC")))
 	runApplication<ExWebmvcApplication>(*args)
 }
